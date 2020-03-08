eacdbaca38848be7a38d9260c5b3e114eebeb69f
Taylor Osborn <osborn.taylor@gmail.com>
2020-03-08 12:26:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -15,14 +16,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  MONGODB_URI ||
+    "mongodb://budget:tracker1@ds133202.mlab.com:33202/heroku_90s5g5jl",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App listening on : http://localhost:${PORT}`);
 });
